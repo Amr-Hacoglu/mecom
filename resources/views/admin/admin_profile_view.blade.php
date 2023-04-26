885842e4ffb7158a55f64c34db2463926d3b5f12
Amr-Hacoglu <omer.abdulw221@gmail.com>
2023-04-26 23:54:17 +0300

    121. Admin Profile & Image Update Part 4

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -55,6 +55,8 @@
 							<div class="col-lg-8">
                                 <div class="card">
                                     <div class="card-body">
+                                    <form method="post" action="{{ route('admin.profile.store') }}" enctype="multipart/form-data" >
+			                        @csrf
                                         <div class="row mb-3">
                                             <div class="col-sm-3">
                                                 <h6 class="mb-0">User Name</h6>
@@ -69,7 +71,7 @@
                                                 <h6 class="mb-0">Full Name</h6>
                                             </div>
                                             <div class="col-sm-9 text-secondary">
-                                                <input type="text" class="form-control" value="{{ $adminData->name }}" />
+                                                <input type="text" name="name" class="form-control" value="{{ $adminData->name }}" />
                                             </div>
                                         </div>
                                         
@@ -78,7 +80,7 @@
                                                 <h6 class="mb-0">Email</h6>
                                             </div>
                                             <div class="col-sm-9 text-secondary">
-                                                <input type="text" class="form-control" value="{{ $adminData->email }}" />
+                                                <input type="email" name="email" class="form-control" value="{{ $adminData->email }}" />
                                             </div>
                                         </div>
 
@@ -87,7 +89,7 @@
                                                 <h6 class="mb-0">Phone </h6>
                                             </div>
                                             <div class="col-sm-9 text-secondary">
-                                                <input type="text" class="form-control" value="{{ $adminData->phone }}" />
+                                                <input type="text" name="phone" class="form-control" value="{{ $adminData->phone }}" />
                                             </div>
                                         </div>
 
@@ -96,7 +98,7 @@
                                                 <h6 class="mb-0">Address</h6>
                                             </div>
                                             <div class="col-sm-9 text-secondary">
-                                                <input type="text" class="form-control" value="{{ $adminData->address }}" />
+                                                <input type="text" name="address" class="form-control" value="{{ $adminData->address }}" />
                                             </div>
                                         </div>
 
@@ -105,7 +107,7 @@
                                                 <h6 class="mb-0">Photo</h6>
                                             </div>
                                             <div class="col-sm-9 text-secondary">
-                                            <input type="file" class="form-control"  id="image"   />
+                                            <input type="file" name="photo" class="form-control" id="image" />
                                             </div>
                                         </div>
 
@@ -121,10 +123,11 @@
                                         <div class="row">
                                             <div class="col-sm-3"></div>
                                             <div class="col-sm-9 text-secondary">
-                                                <input type="button" class="btn btn-primary px-4" value="Save Changes" />
+                                                <input type="submit" class="btn btn-primary px-4" value="Save Changes" />
                                             </div>
                                         </div>
                                     </div>
+                                    </form>
                                 </div>
 							</div>
 						</div>
