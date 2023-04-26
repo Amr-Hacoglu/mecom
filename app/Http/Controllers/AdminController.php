e175e9549df2f7b5eebd3e5fc6fe33d8866e872d
Amr-Hacoglu <omer.abdulw221@gmail.com>
2023-04-26 18:35:13 +0300

    118- Admin Profile & Image Update part 1

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AdminController extends Controller
 {
@@ -30,4 +31,12 @@ class AdminController extends Controller
 
         return redirect('/admin/login'); //when we login with admin, then when we logout with admin it will return you to admin/login page
     } // End Mehtod 
+
+    public function AdminProfile(){
+
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+        return view('admin.admin_profile_view',compact('adminData'));
+
+    } // End Mehtod
 }
