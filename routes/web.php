57cd4da7956885188bee68d288bd6a0031ea5a4b
Amr-Hacoglu <omer.abdulw221@gmail.com>
2023-05-10 19:21:53 +0300

    -143. Brand Page Design Part 1 (Admin)

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\VendorController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\Backend\BrandController;
 
 use function Pest\Laravel\get;
 
@@ -71,3 +72,18 @@ Route::get('/admin/login', [AdminController::class, 'AdminLogin']);
 Route::get('/vendor/login', [VendorController::class, 'VendorLogin']);
 
 require __DIR__.'/auth.php';
+
+Route::middleware(['auth','role:admin'])->group(function() {
+
+
+    // Brand All Route 
+   Route::controller(BrandController::class)->group(function(){
+       Route::get('/all/brand' , 'AllBrand')->name('all.brand');
+   
+   });
+   
+   
+   
+   
+   
+   }); // End Middleware 
