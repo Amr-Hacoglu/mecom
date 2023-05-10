3d022f09169227ff2cdc2f35de8ef4ff4d808d3f
Amr-Hacoglu <omer.abdulw221@gmail.com>
2023-05-10 12:35:15 +0300

    -136. User Profile Design Part 2

--- routes/web.php
+++ routes/web.php
@@ -30,18 +30,15 @@ Route::middleware(['auth'])->group(function() {
     
     }); // Gorup Milldeware End
     
-    
-    // Route::get('/dashboard', function () {
-    //     return view('dashboard');
-    // })->middleware(['auth'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-require __DIR__.'/auth.php';
 
 //Admin Dashboard
 
@@ -70,3 +67,5 @@ Route::middleware(['auth','role:vendor'])->group(function(){
 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin']);
 Route::get('/vendor/login', [VendorController::class, 'VendorLogin']);
+
+require __DIR__.'/auth.php';
