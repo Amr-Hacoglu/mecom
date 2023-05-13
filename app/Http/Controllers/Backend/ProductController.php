e6bf43f1448b7b31403c68c39995fde038345f2d
Amr-Hacoglu <omer.abdulw221@gmail.com>
2023-05-13 16:41:32 +0300

    -171. Load Category Brand Subcategory Vendor Part1

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -9,7 +9,9 @@ use App\Models\SubCategory;
 use App\Models\MultiImg;
 use App\Models\Brand;
 use App\Models\Product;
+use App\Models\User;
 use Image;
+
 
 class ProductController extends Controller
 {
@@ -21,7 +23,10 @@ class ProductController extends Controller
 
     public function AddProduct(){
 
-        return view('backend.product.product_add');
+        $activeVendor = User::where('status','active')->where('role','vendor')->latest()->get();
+        $brands = Brand::latest()->get();
+        $categories = Category::latest()->get();
+        return view('backend.product.product_add',compact('brands','categories','activeVendor'));
 
     } // End Method 
 
