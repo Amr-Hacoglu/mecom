72c7b99f74c734e3624d27e3b75873b32b9d0443
Amr-Hacoglu <omer.abdulw221@gmail.com>
2023-05-23 20:17:06 +0300

    -241. Mini Cart Read Cart Product With Ajax Part 1

--- resources/views/frontend/master_dashboard.blade.php
+++ resources/views/frontend/master_dashboard.blade.php
@@ -188,6 +188,20 @@
     /// End Add To Cart Prodcut 
  </script>
 
+<script type="text/javascript">
+    
+    function miniCart(){
+       $.ajax({
+           type: 'GET',
+           url: '/product/mini/cart',
+           dataType: 'json',
+           success:function(response){
+               console.log(response)
+           }
+       })
+    }
+   </script>
+
 </body>
 
 </html>
