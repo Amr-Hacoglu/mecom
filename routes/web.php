1afc793630401429c3d47690698174e4256e5f65
Amr-Hacoglu <omer.abdulw221@gmail.com>
2023-05-27 16:16:32 +0300

    -282. Checkout Page Design Part 2

--- routes/web.php
+++ routes/web.php
@@ -279,6 +279,15 @@ Route::get('/coupon-remove', [CartController::class, 'CouponRemove']);
 // Checkout Page Route 
 Route::get('/checkout', [CartController::class, 'CheckoutCreate'])->name('checkout');
 
+// Cart All Route 
+Route::controller(CartController::class)->group(function(){
+    Route::get('/mycart' , 'MyCart')->name('mycart');
+    Route::get('/get-cart-product' , 'GetCartProduct');
+    Route::get('/cart-remove/{rowId}' , 'CartRemove');
+    Route::get('/cart-decrement/{rowId}' , 'CartDecrement');
+    Route::get('/cart-increment/{rowId}' , 'CartIncrement');
+}); 
+
 /// User All Route
 Route::middleware(['auth','role:user'])->group(function() {
 
@@ -297,17 +306,6 @@ Route::middleware(['auth','role:user'])->group(function() {
         Route::get('/get-compare-product' , 'GetCompareProduct');
         Route::get('/compare-remove/{id}' , 'CompareRemove');
    }); 
-   
-   
- // Cart All Route 
- Route::controller(CartController::class)->group(function(){
-    Route::get('/mycart' , 'MyCart')->name('mycart');
-    Route::get('/get-cart-product' , 'GetCartProduct');
-    Route::get('/cart-remove/{rowId}' , 'CartRemove');
-    Route::get('/cart-decrement/{rowId}' , 'CartDecrement');
-    Route::get('/cart-increment/{rowId}' , 'CartIncrement');
-}); 
-
 
 
 
