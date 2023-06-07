226ea7870655381ce21967a4454a41341c22c079
Amr-Hacoglu <omer.abdulw221@gmail.com>
2023-06-07 16:34:28 +0300

    -307. View Users Order In Profile Part 1

--- resources/views/frontend/userdashboard/user_order_page.blade.php
+++ resources/views/frontend/userdashboard/user_order_page.blade.php
@@ -62,11 +62,9 @@
 @endif
 
 
-            </td>
-
-
-     <td><a href="#" class="btn-sm btn-success"><i class="fa fa-eye"></i> View</a>
-     <a href="#" class="btn-sm btn-danger"><i class="fa fa-download"></i> Invoice</a>
+        </td>
+            <td><a href="{{ url('user/order_details/'.$order->id) }}" class="btn-sm btn-success"><i class="fa fa-eye"></i> View</a>
+            <a href="#" class="btn-sm btn-danger"><i class="fa fa-download"></i> Invoice</a>
             </td>
         </tr>
         @endforeach
