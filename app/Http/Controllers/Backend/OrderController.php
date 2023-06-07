5a4270641947948f47aa280d9581ee4b59e29a7c
Amr-Hacoglu <omer.abdulw221@gmail.com>
2023-06-07 22:52:30 +0300

    -313. Admin Panel View Details Part 1

--- app/Http/Controllers/Backend/OrderController.php
+++ app/Http/Controllers/Backend/OrderController.php
@@ -18,6 +18,14 @@ class OrderController extends Controller
         return view('backend.orders.pending_orders',compact('orders'));
     } // End Method 
 
+    public function AdminOrderDetails($order_id){
 
+        $order = Order::with('division','district','state','user')->where('id',$order_id)->first();
+        $orderItem = OrderItem::with('product')->where('order_id',$order_id)->orderBy('id','DESC')->get();
+
+        return view('backend.orders.admin_order_details',compact('order','orderItem'));
+
+    }// End Method 
+    
 
 }
--- routes/web.php
+++ routes/web.php
@@ -252,7 +252,7 @@ Route::middleware(['auth','role:admin'])->group(function() {
     // Admin Order All Route 
     Route::controller(OrderController::class)->group(function(){
         Route::get('/pending/order' , 'PendingOrder')->name('pending.order');
-
+        Route::get('/admin/order/details/{order_id}' , 'AdminOrderDetails')->name('admin.order.details');
 
     }); 
 
