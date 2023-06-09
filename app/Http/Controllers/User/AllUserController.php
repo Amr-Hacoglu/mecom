0b6a3e3df508fa0438bd078711ed93551636ee8f
Amr-Hacoglu <omer.abdulw221@gmail.com>
2023-06-09 19:15:23 +0300

    -319. User Profile Send Return Order Request Part1

--- app/Http/Controllers/User/AllUserController.php
+++ app/Http/Controllers/User/AllUserController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Hash;
 use App\Models\Order;
 use App\Models\OrderItem;
 use Barryvdh\DomPDF\Facade\Pdf;
+use Carbon\Carbon;
 
 class AllUserController extends Controller
 {
@@ -54,4 +55,21 @@ class AllUserController extends Controller
 
     }// End Method 
     
+    public function ReturnOrder(Request $request,$order_id){
+
+        Order::findOrFail($order_id)->update([
+            'return_date' => Carbon::now()->format('d F Y'),
+            'return_reason' => $request->return_reason,
+            'return_order' => 1, 
+        ]);
+
+        $notification = array(
+            'message' => 'Return Request Send Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('user.order.page')->with($notification); 
+
+    }// End Method 
+
 }
