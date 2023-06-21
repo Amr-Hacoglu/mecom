94837b8c23937c4b7cec4ac0287e7acbd8d7da20
Amr-Hacoglu <omer.abdulw221@gmail.com>
2023-06-21 13:40:03 +0300

    -340. Blog Post Setup Part 3

--- routes/web.php
+++ routes/web.php
@@ -313,9 +313,9 @@ Route::middleware(['auth','role:admin'])->group(function() {
         Route::get('/admin/blog/post' , 'AllBlogPost')->name('admin.blog.post'); 
         Route::get('/admin/add/blog/post' , 'AddBlogPost')->name('add.blog.post');
         Route::post('/admin/store/blog/post' , 'StoreBlogPost')->name('store.blog.post');
-        Route::get('/admin/edit/blog/category/{id}' , 'EditBlogCateogry')->name('edit.blog.category');
-        Route::post('/admin/update/blog/category' , 'UpdateBlogCateogry')->name('update.blog.category');
-        Route::get('/admin/delete/blog/category/{id}' , 'DeleteBlogCateogry')->name('delete.blog.category');
+        Route::get('/admin/edit/blog/post/{id}' , 'EditBlogPost')->name('edit.blog.post');
+        Route::post('/admin/update/blog/post' , 'UpdateBlogPost')->name('update.blog.post');
+        Route::get('/admin/delete/blog/post/{id}' , 'DeleteBlogPost')->name('delete.blog.post');
    
    });
 
