079fdb29804c22ab2ab27ef6a38729219caa3899
Amr-Hacoglu <omer.abdulw221@gmail.com>
2023-06-24 21:44:21 +0300

    -365. Order Tracking By Invoice Part 1

--- routes/web.php
+++ routes/web.php
@@ -454,6 +454,8 @@ Route::middleware(['auth','role:user'])->group(function() {
         Route::get('/user/invoice_download/{order_id}' , 'UserOrderInvoice');
         Route::post('/return/order/{order_id}' , 'ReturnOrder')->name('return.order');
         Route::get('/return/order/page' , 'ReturnOrderPage')->name('return.order.page');
+         // Order Tracking 
+        Route::get('/user/track/order' , 'UserTrackOrder')->name('user.track.order');
     }); 
 
 
