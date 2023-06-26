aa1d20dba9f5eb724d21a34e86a7d926d7064b15
Amr-Hacoglu <omer.abdulw221@gmail.com>
2023-06-26 16:43:45 +0300

    -382. Add Roles in Permission Part 4

--- routes/web.php
+++ routes/web.php
@@ -368,6 +368,8 @@ Route::middleware(['auth','role:admin'])->group(function() {
         // add role permission 
         Route::get('/add/roles/permission' , 'AddRolesPermission')->name('add.roles.permission');
         Route::post('/role/permission/store' , 'RolePermissionStore')->name('role.permission.store');
+        Route::get('/all/roles/permission' , 'AllRolesPermission')->name('all.roles.permission');
+        Route::get('/admin/edit/roles/{id}' , 'AdminRolesEdit')->name('admin.edit.roles');
     });
 
 }); // Admin End Middleware 
