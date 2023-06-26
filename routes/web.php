0405e6cdff6acd4c1536720a7012fd1202f21b9f
Amr-Hacoglu <omer.abdulw221@gmail.com>
2023-06-26 12:56:43 +0300

    -377. Setup User Roles Part 1

--- routes/web.php
+++ routes/web.php
@@ -346,7 +346,7 @@ Route::middleware(['auth','role:admin'])->group(function() {
         Route::post('/seo/setting/update' , 'SeoSettingUpdate')->name('seo.setting.update');
     });    
 
-    // Site Setting All Route 
+    // Permission All Route 
     Route::controller(RoleController::class)->group(function(){
         Route::get('/all/permission' , 'AllPermission')->name('all.permission');
         Route::get('/add/permission' , 'AddPermission')->name('add.permission');
@@ -354,6 +354,18 @@ Route::middleware(['auth','role:admin'])->group(function() {
         Route::get('/edit/permission/{id}' , 'EditPermission')->name('edit.permission');
         Route::post('/update/permission' , 'UpdatePermission')->name('update.permission');
         Route::get('/delete/permission/{id}' , 'DeletePermission')->name('delete.permission');
+    });
+
+    // Roles All Route 
+    Route::controller(RoleController::class)->group(function(){
+
+        Route::get('/all/roles' , 'AllRoles')->name('all.roles');
+        Route::get('/add/roles' , 'AddRoles')->name('add.roles');
+        Route::post('/store/roles' , 'StoreRoles')->name('store.roles');
+        Route::get('/edit/permission/{id}' , 'EditPermission')->name('edit.permission');
+        Route::post('/update/permission' , 'UpdatePermission')->name('update.permission');
+        Route::get('/delete/permission/{id}' , 'DeletePermission')->name('delete.permission');
+    
     });
 
 }); // Admin End Middleware 
