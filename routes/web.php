77edad769bc46d251fc041d09c5e2edf2d1f4ca5
Amr-Hacoglu <omer.abdulw221@gmail.com>
2023-06-27 13:00:01 +0300

    -387  Multi Admin Setup Part 3

--- routes/web.php
+++ routes/web.php
@@ -379,6 +379,7 @@ Route::middleware(['auth','role:admin'])->group(function() {
 
         Route::get('/all/admin' , 'AllAdmin')->name('all.admin');
         Route::get('/add/admin' , 'AddAdmin')->name('add.admin');
+        Route::post('/admin/user/store' , 'AdminUserStore')->name('admin.user.store');
     });
 
 }); // Admin End Middleware 
