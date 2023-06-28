573bbf51410661a480c944bc33d284e16cceb5f8
Amr-Hacoglu <omer.abdulw221@gmail.com>
2023-06-28 12:52:50 +0300

    -393. Setup Notifications in Admin Part 1

--- app/Http/Controllers/User/StripeController.php
+++ app/Http/Controllers/User/StripeController.php
@@ -12,6 +12,9 @@ use Carbon\Carbon;
 use Auth;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\OrderMail;
+use App\Models\User;
+use App\Notifications\OrderComplete;
+use Illuminate\Support\Facades\Notification;
 
 class StripeController extends Controller
 {
@@ -122,6 +125,7 @@ class StripeController extends Controller
 
     public function CashOrder(Request $request){
 
+        $user = User::where('role','admin')->get();
         if(Session::has('coupon')){
             $total_amount = Session::get('coupon')['total_amount'];
         }else{
@@ -201,7 +205,7 @@ class StripeController extends Controller
                 'message' => 'Your Order Place Successfully',
                 'alert-type' => 'success'
             );
-
+            Notification::send($user, new OrderComplete($request->name));
             return redirect()->route('dashboard')->with($notification); 
 
 
